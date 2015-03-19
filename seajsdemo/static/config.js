7775a982b23815c50416e892c5d727398b5dbe9c
Shawn <shawnzhang009@gmail.com>
2015-03-19 10:26:05 +0800

    test

---
+++ seajsdemo/static/config.js
@@ -0,0 +1,46 @@
+var version = '20140414';
+var versionJS = '20150106';
+
+seajs.config({
+    base: "../static/js/",
+    alias: {
+        // 当模块标识很长时，可以使用 alias 来简化
+        'jquery': 'jquery-min.js',
+        'general':'general.js'
+            /*        'asyncSlider': 'lib/asyncSlider/1.0.0/asyncSlider.js',
+                    'city': 'lib/city/1.0.0/city.js',
+                    'new-city': 'lib/city/1.0.1/new-city.js',
+                    'counter': 'lib/counter/0.0.1/jquery.counter.js',
+                    'easing': 'lib/easing/1.3.0/easing.js',
+                    'flash': 'lib/flash/1.0.1/jquery.flash.js',
+                    'handlebars': 'lib/handlebars/1.0.0/handlebars.js',
+                    'highcharts': 'lib/highcharts/3.0.5/highcharts.js',
+                    'jquery': 'lib/jquery/1.9.1/jquery.js',
+                    'queue': 'lib/plupload/1.5.7/jquery.plupload.queue',
+                    'simplePagination': 'lib/simplePagination/1.5.0/simplePagination.js',
+                    'validate': 'lib/validation/1.11.1/jquery.validate.js',
+                    'mailSuggest': 'lib/mailSuggest/0.0.1/mailSuggest.js',
+                    'rsa': 'rsa/index.js',
+
+                    'base': 'arale/base/1.1.1/base.js',
+                    'class': 'arale/class/1.1.0/class.js',
+                    'events': 'arale/events/1.1.0/events',
+                    'widget': 'arale/widget/1.1.1/widget.js',
+                    'popup': 'arale/popup/1.1.2/popup.js',
+                    'tip': 'arale/tip/1.1.4/tip.js',
+                    'overlay': 'arale/overlay/1.1.2/overlay.js',
+                    'mask': 'arale/overlay/1.1.2/mask.js',
+                    'iframe-shim': 'arale/shim/1.0.2/iframe-shim.js',
+                    'position': 'arale/position/1.0.1/position.js',
+                    'dialog': 'arale/dialog/1.3.3/dialog.js',
+                    'confirmbox': 'arale/dialog/1.3.3/confirmbox.js',
+                    'templatable': 'arale/templatable/0.9.1/templatable.js',
+                    'calendar': 'arale/calendar/0.9.0/calendar.js',
+                    'moment': 'arale/moment/2.1.0/moment.js',
+                    'sticky': 'arale/sticky/1.2.1/sticky.js',
+
+                    'ui-counter': 'lib/counter/0.0.1/jquery.counter-analog.css',
+                    'ui-poptip': 'alice/poptip/1.1.1/poptip.css'*/
+    },
+
+});
---
+++ seajsdemo/static/js/pages/index.js
@@ -0,0 +1,14 @@
+// 所有模块都通过 define 来定义
+define(function(require, exports, module) {
+
+  // 通过 require 引入依赖
+  var $ = require('jquery');
+  var Spinning = require('./spinning');
+
+/*  // 通过 exports 对外提供接口
+  exports.doSomething = ...
+
+  // 或者通过 module.exports 提供整个接口
+  module.exports = ...*/
+
+});
