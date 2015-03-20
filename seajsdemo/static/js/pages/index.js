0c8b725cf7d7868cc3ca5e842a63ba1e8d1c0ea8
Shawn <shawnzhang009@gmail.com>
2015-03-20 17:52:21 +0800

    add demos of sea.js

--- seajsdemo/static/js/pages/index.js
+++ seajsdemo/static/js/pages/index.js
@@ -1,14 +1,57 @@
 // 所有模块都通过 define 来定义
-define(function(require, exports, module) {
+alert('fwefw'); define(function(require, exports, module) {
 
-  // 通过 require 引入依赖
-  var $ = require('jquery');
-  var Spinning = require('./spinning');
+	// 通过 require 引入依赖
+	var $ = require('jquery');
+	/*var Spinning = require('./spinning');*/
+	var obj = {
+		myString: "Hello world!",
+		myFunc: function() {
+			return this.myString;
+		}
+	};
 
-/*  // 通过 exports 对外提供接口
-  exports.doSomething = ...
+	/*module.exports = new obj;*/
+	/*	function heee() {
+			$('#d1').css('background', 'black');
+		}
+		module.exports = {
+			name: 'hello',
+			method: function() {
+				alert('method on ');
+			}
+		};*/
 
-  // 或者通过 module.exports 提供整个接口
-  module.exports = ...*/
+	/*exports.doSomething = function s() {
+		alert('fe');
+	}*/
+
+	/*	try {
+			var obj = document.getElementById('d1');
+			var set = {
+				changeColor: function() {
+					obj.style.color = 'green';
+				},
+				changeWidth: function() {
+					obj.style.width = '323px;'
+				}
+			}
+
+			function init() {
+				set.changeColor();
+				alert('fwe');
+			}
+		} catch (err) {
+			alert(err);
+		}*/
+
+	/*
+		exports.ttt = test();*/
+	/*  // 通过 exports 对外提供接口
+	  exports.doSomething = ...
+
+	  // 或者通过 module.exports 提供整个接口
+	  module.exports = ...*/
+
 
 });
