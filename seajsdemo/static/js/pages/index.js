5201d2b54be5efeefc9727aa92e3f5203fdfb364
Shawn <shawnzhang009@gmail.com>
2015-03-23 18:02:36 +0800

    sea.js is working

--- seajsdemo/static/js/pages/index.js
+++ seajsdemo/static/js/pages/index.js
@@ -1,57 +1,18 @@
 // 所有模块都通过 define 来定义
-alert('fwefw'); define(function(require, exports, module) {
+define(function(require) {
 
 	// 通过 require 引入依赖
 	var $ = require('jquery');
-	/*var Spinning = require('./spinning');*/
-	var obj = {
-		myString: "Hello world!",
-		myFunc: function() {
-			return this.myString;
-		}
-	};
+	/*var index = require('./index');*/
 
-	/*module.exports = new obj;*/
-	/*	function heee() {
-			$('#d1').css('background', 'black');
-		}
-		module.exports = {
-			name: 'hello',
-			method: function() {
-				alert('method on ');
-			}
-		};*/
+	function Main(container) {
+		this.container = $(container);
+	}
 
-	/*exports.doSomething = function s() {
-		alert('fe');
-	}*/
+	Main.prototype.render = function() {
+		this.container.css("background", "green");
+	}
 
-	/*	try {
-			var obj = document.getElementById('d1');
-			var set = {
-				changeColor: function() {
-					obj.style.color = 'green';
-				},
-				changeWidth: function() {
-					obj.style.width = '323px;'
-				}
-			}
-
-			function init() {
-				set.changeColor();
-				alert('fwe');
-			}
-		} catch (err) {
-			alert(err);
-		}*/
-
-	/*
-		exports.ttt = test();*/
-	/*  // 通过 exports 对外提供接口
-	  exports.doSomething = ...
-
-	  // 或者通过 module.exports 提供整个接口
-	  module.exports = ...*/
-
-
+	var s = new Main("#div1");
+	s.render();
 });
