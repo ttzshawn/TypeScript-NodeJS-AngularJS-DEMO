3d616da56e37bdcd6e27f32f9e5509380d6de401
Shawn.Zhang <shawnzhang009@gmail.com>
2016-03-31 02:36:28 +0800

    fe

---
+++ OS_Angular/src/public/js/core/login-panel.controller.js
@@ -0,0 +1,65 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.components')
+        .controller('LoginCtrl', LoginCtrl);
+
+    LoginCtrl.$inject = ['$scope', '$rootScope', '$location', '$http','$window', '$cookies', '$state', 'AUTH_EVENTS', 'CommonService', 'Session', 'AuthService', 'Login'];
+
+    /* @ngInject */
+    function LoginCtrl($scope, $rootScope, $location, $http, $window,$cookies, $state, AUTH_EVENTS, CommonService, Session, AuthService, Login) {
+
+        // if (!!$rootScope.currentUser) {
+        //     $state.go('home');
+        // }
+        // for test
+        $scope.toggleMO = function() {
+            $(this).addClass("al")
+            console.log(this)
+            $('.mo-list').animate({
+                width: 'toggle'
+            }, 350);
+        }
+
+        $scope.user = {};
+        $scope.login = function(user) {
+
+            Login.get({}, function(res) {
+                console.log(res)
+                $window.sessionStorage.token = res.token || '';
+                $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+                $state.go('home.order');
+            }, function(res) {
+                alert('login failed.');
+            });
+            // console.log(aaa);
+            // console.log(aaa.token);
+            // $scope.msg = Login.check(user, function(res) {
+            //     console.log(res.token);
+            // });
+
+
+            // AuthService.login(user).then(function(res) {
+            //
+            //     console.log(res);
+            //     if (CommonService.isReqSuccess(res)) {
+            //         console.log('login success');
+            //         // $cookies.put(res.data.sessionid, user.accountname);
+            //         Session.create(res.data.sessionid, user.accountname);
+            //         $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+            //         $state.go('home.order');
+            //         $rootScope.setCurrentUser(user.accountname);
+            //     } else {
+            //         CommonService.handleResErr(res);
+            //         $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+            //     }
+            // }, function(res) {
+            //     CommonService.handleHttpErr(res);
+            //     $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+            // });
+            console.log(user);
+        }
+    }
+
+})();
