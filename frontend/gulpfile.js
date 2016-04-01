697dd33b9ae15b0716375e7a0ac25433b7dfee51
Shawn Zhang <shawnzhang009@gmail.com>
2016-04-01 19:33:04 +0800

    add usemin and gulp

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,13 +1,16 @@
 // Require gulp,compass
 const gulp = require('gulp');
 const compass = require('gulp-compass');
+const usemin = require('gulp-usemin');
 const concat = require("gulp-concat");
 const connect = require("gulp-connect");
 const minifyJs = require("gulp-uglify");
+const minifyCss = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 
 const paths = {
     base: './',
+    index: './src/index.html'
     jsModule: './src/public/js/**/*.module.js',
     js: './src/public/js/**/*.js',
     sass: './src/public/sass/**/*.scss',
@@ -30,6 +33,18 @@ gulp.task('scripts', () => {
         // .pipe(minifyJs())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./src/public/js/'));
+});
+
+// Handle components from index
+gulp.task('usemin', function() {
+    return gulp.src(paths.index)
+        .pipe(usemin({
+            js: [minifyJs(), 'concat'],
+            css: [minifyCss({
+                keepSpecialComments: 0
+            }), 'concat'],
+        }))
+        .pipe(gulp.dest('../static/'));
 });
 
 // Compile SASS
